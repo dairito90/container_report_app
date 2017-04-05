2abbb1a0d7c5c58db493ebda36f73cdbd0a4b31a
Dairon Rodriguez <drodriguez@fvi.edu>
2017-04-05 17:30:27 -0400

    starting app

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+var module = angular.module("ContainerApp", []);
+module.controller('ContainerReportCtrl', ['$scope',function($scope){
+  $scope.items = {
+      $scope.company ="";
+      $scope.container# ="";
+      $scope.arrivaldate ="";
+      $scope.departureday ="";
+      $scope.numberofpieces = "";
+  }
+
+
+
+}]);
+})();
