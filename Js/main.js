6f56dffeb504e3e26d58374a219c15f846d47257
Dairon Rodriguez <drodriguez@fvi.edu>
2017-04-06 00:43:54 -0400

    starting write js file

--- Js/main.js
+++ Js/main.js
@@ -1,14 +1,33 @@
-var module = angular.module("ContainerApp", []);
-module.controller('ContainerReportCtrl', ['$scope',function($scope){
-  $scope.items = {
-      $scope.company ="";
-      $scope.container# ="";
-      $scope.arrivaldate ="";
-      $scope.departureday ="";
-      $scope.numberofpieces = "";
-  }
+var app = angular.module("ContainerApp", []);
+app.controller('ContainerReportCtrl', ['$scope',function($scope){
+ // $scope.company= '';
+ // $scope.containernumber= '';
+ // $scope.arrivaldate= '';
+ // $scope.departureday= '';
+ // $scope.numberofpieces= '';
+  var items=[];
+ $scope.handleclick=function(){
+     console.log("hi");
+     var item= {
+         company:$scope.company,
+         containernumber:$scope.containernumber,
+         arrivaldate:$scope.arrivaldate,
+         departureday:$scope.departureday,
+         numberofpieces:$scope.numberofpieces
+     }
+     $scope.items.push(item);
+     $scope.company="";
+     $scope.containernumber= "";
+     $scope.arrivaldate= "";
+     $scope.departureday= "";
+     $scope.numberofpieces= "";
+
+ }
+
+
+
+
 
 
 
 }]);
-})();
